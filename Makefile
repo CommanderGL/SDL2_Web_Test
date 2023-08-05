3cb1b7634b8f7034136049287051b81c3ee79b8a
Grady Link <crossscar2@outlook.com>
2023-08-05 22:48:32 +0000

    Add Watch and new shell

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 SOURCE_DIR = src
 OUT_DIR = bin
 
+SHELL_FILE_PROD = shell_prod.html
+SHELL_FILE_DEV = shell_dev.html
+
 OBJS = main.cpp
 OBJ_NAME = test
 
@@ -8,7 +11,16 @@ CC = em++
 
 COMPILER_FLAGS = -g -lm --bind -s USE_SDL=2
 
-all: build
+NODE_PACKAGE_MANAGER = pnpm # For Watch Mode
 
-build:
-	$(CC) $(SOURCE_DIR)/$(OBJS) -o $(OUT_DIR)/$(OBJ_NAME).html $(COMPILER_FLAGS)
+build_prod:
+	$(CC) $(SOURCE_DIR)/$(OBJS) -o $(OUT_DIR)/$(OBJ_NAME).html --shell-file $(SHELL_FILE_PROD) $(COMPILER_FLAGS)
+
+build_dev:
+	$(CC) $(SOURCE_DIR)/$(OBJS) -o $(OUT_DIR)/$(OBJ_NAME).html --shell-file $(SHELL_FILE_DEV) $(COMPILER_FLAGS)
+
+watch_prod:
+	$(NODE_PACKAGE_MANAGER) nodemon -w src/**/* -w $(SHELL_FILE_PROD) -e cpp,c,h,hpp,html -x "make build_prod || exit 0"
+
+watch_dev:
+	$(NODE_PACKAGE_MANAGER) nodemon -w src/**/* -w $(SHELL_FILE_DEV) -e cpp,c,h,hpp,html -x "make build_dev || exit 0"
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
+
 SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
 
 SDL_Event e;
 bool quit = false;
@@ -12,6 +14,9 @@ void update() {
 	while (SDL_PollEvent(&e)) {
 		if( e.type == SDL_QUIT ) quit = true;
 	}
+
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00));
+	SDL_UpdateWindowSurface(window);
 
 	if (quit) {
 		emscripten_cancel_main_loop();
@@ -22,25 +27,11 @@ void update() {
 }
 
 int main(int argc, char* args[]) {
-	SDL_Surface* screenSurface = NULL;
 	SDL_Init(SDL_INIT_EVERYTHING);
 	window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	screenSurface = SDL_GetWindowSurface(window);
 
-	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00));
-	SDL_UpdateWindowSurface(window);
-
 	emscripten_set_main_loop(update, 0, 1);
 
-	/* SDL_Event e;
-	bool quit = false;
-	while (!quit) {
-		while (SDL_PollEvent(&e)) {
-			if( e.type == SDL_QUIT ) quit = true;
-		}
-	} */
-
-	/* SDL_DestroyWindow(window);
-	SDL_Quit(); */
 	return 0;
 }
